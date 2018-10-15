f4523229db34a33bc2bd17dff3bf9a61cae240b4
Shannon Sloan <shannonsloan303@gmail.com>
2018-10-15 13:50:43 -0400

    "Modified Main Activity-Tutorial 2"

--- app/src/main/java/com/example/ses7578/helloworld/ses7578.java
+++ app/src/main/java/com/example/ses7578/helloworld/ses7578.java
@@ -1,27 +1,37 @@
 package com.example.ses7578.helloworld;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
-public class ses7578 extends AppCompatActivity {
+public class ses7578 extends AppCompatActivity
+{
+    private Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ses7578);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+        button3 = (Button) findViewById(R.id.button3);
+
+        button3.setOnClickListener(new View.OnClickListener(){
+           public void onClick(View v)
+           {
+               openMainActivity();
+           }
         });
+
     }
+
+    public void openMainActivity()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
